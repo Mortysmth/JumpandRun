2bbab4325c6bcbe674017693712dc3ca3aa75685
Jonas <111230046+Mrxmouds@users.noreply.github.com>
2022-09-04 20:45:27 +0200

    Ich muss ein Bild für einen Charakter haben, übernahme ist implementiert

--- player.py
+++ player.py
@@ -4,8 +4,10 @@ from support import import_folder
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
-        self.image =pygame.Surface((32,64))
-        self.image.fill('red')
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations['idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft = pos)
 
         #player movement
@@ -15,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed = -16
 
     def import_character_assets(self):
-        character_path = '../graphics/chracter/'
+        character_path = '../graphics/character/'
         self.animations = {'idle' : [], 'run' : [], 'jump' : [], 'fall' : []}
 
         for animation in self.animations.keys():
